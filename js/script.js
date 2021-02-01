2f5e7986b50d5e79d06cfeef95594e04c21291ef
corner0303 <corner0303@gmail.com>
2021-02-02 00:05:58 +0900

    index.htmlの完成

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$("#hamburger-link").click(function(){
+  $("#gnav").toggleClass("gnav--open")
+  $("#line1").toggleClass("hamburger-icon__part--open1")
+  $("#line2").toggleClass("hamburger-icon__part--open2")
+  $("#line3").toggleClass("hamburger-icon__part--open3")
+})
